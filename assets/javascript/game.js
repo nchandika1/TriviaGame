641e114aa51992f13d52a03c205a0dfc0112e1dc
nchandika1 <nchandika@gmail.com>
2017-10-17 17:03:13 -0700

    few more touches

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,25 +14,64 @@ var trivia2 = {
 	answer: "Three"
 }
 
+// Trivia Array and the Index into the array
 var triviaQuestion = 0;
 var questionArray = [trivia1, trivia2];
+
+// Counters for the score
 var rightCount = 0;
 var wrongCount = 0;
 var unAnsweredCount = 0;
 
-const timeoutSeconds = 5000 //30 seconds
-const timeoutAnswerStatus = 5000 //3 seconds
+/******* TIMER RELATED DECLARATIONS ********/
+/* There are two timers:  
+	Time Remaining:  Runs when the trivia question is displayed
+					 Status goes to unanswered if the timer expires
+	Display Answer Time:  When the player's response is displayed, 
+						  Window is displayed for few seconds before 
+						  moving on to next question
+*/
 
-var triviaQuestionTimer;
-var displayScoreTimer;
+// Constant to define the timer for trivia question
+const timeRemainingInSeconds = 15 //15 seconds
+// Constant to define the timer for result display...
+// before moving onto the next questions
+const answerWindowTimeout = 3000 //3 seconds
 
-var timeRemaining = timeoutSeconds;
+// Timer to store the result display timer
+var displayAnswerTimer;
+
+// Timer to store the trivia countdown timer
+var timeRemainingTimer;
+
+/******************************************/
 
 function resetValues() {
-	triviaQuestion = 0;
-	rightCount = 0;
-	wrongCount = 0;
-	unAnsweredCount = 0;
+	triviaQuestion = 0; // Question Index
+	rightCount = 0; // Right answer count
+	wrongCount = 0; // Wrong answer count
+	unAnsweredCount = 0; // Unanswered count
+}
+
+function displayTimeRemaining() {
+	// Function that displays countdown Timer on the screen
+
+	var seconds = timeRemainingInSeconds;
+	timeRemainingTimer = setInterval(function() {
+		// Attach to the elemnt for display
+		$("#time-remaining").html(seconds);
+		seconds--;
+		if (seconds < 0) {
+			clearInterval(timeRemainingTimer); // Disable Timer
+			unAnsweredCount++; // Update counter
+
+			// Get the next trivia question
+			//??????????????????????
+			// display answer window here with unanswered response
+			displayTriviaResponse("unanswered");
+			//renderNextTriviaQuestion();
+		}
+	}, 1000); // 1 second countdown
 }
 
 function displayFinalScore() {
@@ -43,113 +82,97 @@ function displayFinalScore() {
 	$("#trivia-display").empty();
 
 	$("#trivia-display").html("<div id=\"score-text\"></div>");
-	$("#score-text").html("<p> You final score is:</p>");
-	$("#score-text").append("<p> Correct Answers: " + rightCount + "</p>");
-	$("#score-text").append("<p> Wrong Answers: " + wrongCount + "</p>");
-	$("#score-text").append("<p> Unanswered: " + unAnsweredCount + " </p>");
+	$("#score-text").html("<h3>Your final score is:</h3>");
+	$("#score-text").append("<p>Correct Answers: " + rightCount + "</p>");
+	$("#score-text").append("<p>Wrong Answers: " + wrongCount + "</p>");
+	$("#score-text").append("<p>Unanswered: " + unAnsweredCount + " </p>");
 	$("#score-text").append("<button id=\"start-over-btn\" class=\"start-over\">Start Over</button>");
 
 	$("#start-over-btn").click(function() {
+		// User pressed Start Over button, so start the game again
 		resetValues();
-		getTriviaScreen();
+		renderTriviaQuestion();
 	});
 }
 
-function displayAnswerTimeoutFunc() {
-	console.log("Clear display timer!");	
-	clearTimeout(displayScoreTimer);
+function displayAnswerWindowTimeout() {
+	// Function to execute when the timer expires on the answer window
+	console.log("displayAnswerWindowTimeout");	
 
-	triviaQuestion++;
-	if (triviaQuestion == questionArray.length) {
-		displayFinalScore();
-		console.log("Game Over");
-	} else {
-		getTriviaScreen();
-		console.log("Next question");
-	}
+	// Clear the timer first
+	clearTimeout(displayAnswerTimer);
+
+	// Move on to the next question or reach the end
+	renderNextTriviaQuestion();
 }
 
 function displayTriviaResponse(answer) {
+	// Function creates the screen to indicate to the player the answer
+	// is correct or incorrect before movign on to the next question.
 
 	// Let us empty out the screen first
 	$("#trivia-display").empty();
 
-	if (answer) {
-		console.log("Display right answer details");
-		$("#trivia-display").html("<p><strong>Correct Answer!</strong></p");
-	} else {
-		console.log("Display wrong answer details");
-		$("#trivia-display").html("<p>Incorrect Answer!</p>");
+	switch (answer) {
+		case "correct":
+			$("#trivia-display").html("<p><strong>Correct Answer!</strong></p");
+			break;
+		case "incorrect":
+			$("#trivia-display").html("<p><strong>Incorrect Answer!</strong></p>");
+			$("#trivia-display").append("<p>Correct Answer is : " + questionArray[triviaQuestion].answer + "</p>");
+			break;
+		case "unanswered":
+			$("#trivia-display").html("<p></strong>Time's Up!</strong></p>");
+			$("#trivia-display").append("<p>Correct Answer is : " + questionArray[triviaQuestion].answer + "</p>");
+			break;
 	}
-	//Display for 3s and either move to the 
-	console.log("Start display timer!");
-	displayScoreTimer = setTimeout(displayAnswerTimeoutFunc, timeoutAnswerStatus);
+
+	// Display this screen only for 3s and then move on to the next Trivia
+	// Question after the timer expires.
+	displayAnswerTimer = setTimeout(displayAnswerWindowTimeout, answerWindowTimeout);
 }
 
 function checkAnswer(event) {
 	// Check if the player's selection is the right answer
-
-	// Clear Timer since the user selected an answer
-	console.log("Clear Timer!");
-	clearTimeout(triviaQuestionTimer);
-
 	console.log("checkAnswer: " + event.data.param1);
+
+	// Disable the "Time Remaining" timer since the user selected a choice.
+	clearInterval(timeRemainingTimer);
+
 	var correct_answer = questionArray[triviaQuestion].answer;
-	var choice = event.data.param1;
-	if (choice == correct_answer) {
-		rightCount++;
-		console.log("correct answer!");
-		displayTriviaResponse(true);
+	var player_choice = event.data.param1;
+	if (player_choice == correct_answer) {
+		rightCount++; // Increment correct answer count;
+		console.log("Answer: Correct!");
+
+		// Display to the player that the answer is CORRECT!
+		displayTriviaResponse("correct");
 
 	} else {
 		wrongCount++;
-		console.log("wrong answer!");
-		displayTriviaResponse(false);
+		console.log("Answer: Incorrect!");
 
+		// Display to the player that the answer is INCORRECT!
+		displayTriviaResponse("incorrect");
 	}
-/*
+}
+
+function renderNextTriviaQuestion() {
+	// Function that tries to get the next Trivia question unless it reaches the end
+	// Move on to the next question or reach the end
 	triviaQuestion++;
 	if (triviaQuestion == questionArray.length) {
 		displayFinalScore();
 		console.log("Game Over");
 	} else {
-		getTriviaScreen();
+		renderTriviaQuestion();
 		console.log("Next question");
 	}
-	*/
 }
 
-function timeoutAction() {
-	// Unanswered question:  Timer is out and user didn't select any answer
-
-	console.log("Time's Up!");
-
-	// Clear Timer so we can start when the next trivia is displayed
-	clearTimeout(triviaQuestionTimer);
-
-	// Increment Unanswered count here
-	unAnsweredCount++;
-
-	// Move to the next question or display final score if no more trivias left
-	triviaQuestion++;
-
-	if (triviaQuestion == questionArray.length) {
-		displayFinalScore();
-		console.log("Game Over");
-	} else {
-		displayTriviaResponse(false);
-		getTriviaScreen();
-		console.log("Next question");
-	}
-
-}
-
-function timeRemaining() {
-	timeRemaining--;
-	$("#time-remaining").text(timeRemaining);
-}
-
-function getTriviaScreen() {
+function renderTriviaQuestion() {
+	// Function to display Trivia Question and initiat the click functions	
+	console.log("renderTriviaQuestion #: " + triviaQuestion);
 
 	// Let us empty out the screen first
 	$("#trivia-display").empty();
@@ -165,25 +188,23 @@ function getTriviaScreen() {
 	$("#option-btn").append("<p id=\"option2\">" + questionArray[triviaQuestion].option2 + "</p>");
 	$("#option-btn").append("<p id=\"option3\">" + questionArray[triviaQuestion].option3 + "</p>");
 
-	// Set Timer now
-	console.log("Start timer!");
-	triviaQuestionTimer = setTimeout(timeoutAction, timeoutSeconds);
+	// After displaying the Trivia question, 
+	// set Seconds Remaining Timer
+	displayTimeRemaining();
 	
-	//Click Functions for the Options
-	console.log(questionArray[triviaQuestion].option1);
+	// Click Functions for the Options and pass the button indicator to the function
 	$("#option1").click({param1: questionArray[triviaQuestion].option1}, checkAnswer);
 	$("#option2").click({param1: questionArray[triviaQuestion].option2}, checkAnswer);
 	$("#option3").click({param1: questionArray[triviaQuestion].option3}, checkAnswer);
 }
 
 function startGame() {
-	// Start the game by displaying the welcome screen and a start button
-
 	console.log("startGame");
 	console.log(questionArray);
 
-	getTriviaScreen();
+	// Get the first Trivia question
+	renderTriviaQuestion();
 }
 
-// Main function to start the game
+// Main function:  Run when the "Start" is clicked
 $("#start-button").click(startGame);
